d29307e4985596d8e48849df426ef1483e7ed810
leirof <vince.lrf@gmail.com>
2023-10-31 17:23:15 +0100

    Moved DB query to Database class

--- allay/core/src/database.py
+++ allay/core/src/database.py
@@ -7,6 +7,7 @@
 
 import os
 import sqlite3
+from typing import TYPE_CHECKING, Any, Awaitable, Callable, Dict, List, Optional, Union
 
 # Modules ---------------------------------------------------------------------
 
@@ -50,3 +51,50 @@ class Database:
                     cursor.executescript(file.read())
 
         cursor.close()
+
+    @staticmethod
+    def query(
+        query: str,
+        args: Union[tuple, dict] = [],
+        *,
+        fetchone: bool = False,
+        returnrowcount: bool = False,
+        astuple: bool = False,
+    ) -> Union[int, List[dict], dict]:
+        """
+        Faire une requête à la base de données du bot
+
+        Si SELECT, retourne une liste de résultats, ou seulement le premier résultat (si fetchone)
+        Pour toute autre requête, retourne l'ID de la ligne affectée, ou le nombre de lignes
+        affectées (si returnrowscount)
+
+        :param query: La requête à faire
+        :param args: Les arguments de la requête
+        :param fetchone: Si la requête est un SELECT, retourne seulement le premier résultat
+        :param returnrowcount: Si la requête est un INSERT, UPDATE ou DELETE, retourne le nombre
+            de lignes affectées
+        :param astuple: Si la requête est un SELECT, retourne les résultats sous forme de tuple
+        :return: Le résultat de la requête
+        """
+
+        cursor = Database.database.cursor()
+        try:
+            cursor.execute(query, args)
+            if query.startswith("SELECT"):
+                _type = tuple if astuple else dict
+                if fetchone:
+                    row = cursor.fetchone()
+                    result = _type() if row is None else _type(row)
+                else:
+                    result = list(map(_type, cursor.fetchall()))
+            else:
+                Database.database.commit()
+                if returnrowcount:
+                    result = cursor.rowcount
+                else:
+                    result = cursor.lastrowid
+        except Exception as exception:
+            cursor.close()
+            raise exception
+        cursor.close()
+        return result
--- allay/core/src/discord/bot.py
+++ allay/core/src/discord/bot.py
@@ -116,53 +116,6 @@ class Bot(commands.bot.AutoShardedBot):
 
         return commands.when_mentioned_or(prefix)(self, msg)
 
-    def db_query(
-        self,
-        query: str,
-        args: Union[tuple, dict] = [],
-        *,
-        fetchone: bool = False,
-        returnrowcount: bool = False,
-        astuple: bool = False,
-    ) -> Union[int, List[dict], dict]:
-        """
-        Faire une requête à la base de données du bot
-
-        Si SELECT, retourne une liste de résultats, ou seulement le premier résultat (si fetchone)
-        Pour toute autre requête, retourne l'ID de la ligne affectée, ou le nombre de lignes
-        affectées (si returnrowscount)
-
-        :param query: La requête à faire
-        :param args: Les arguments de la requête
-        :param fetchone: Si la requête est un SELECT, retourne seulement le premier résultat
-        :param returnrowcount: Si la requête est un INSERT, UPDATE ou DELETE, retourne le nombre
-            de lignes affectées
-        :param astuple: Si la requête est un SELECT, retourne les résultats sous forme de tuple
-        :return: Le résultat de la requête
-        """
-
-        cursor = self.database.cursor()
-        try:
-            cursor.execute(query, args)
-            if query.startswith("SELECT"):
-                _type = tuple if astuple else dict
-                if fetchone:
-                    row = cursor.fetchone()
-                    result = _type() if row is None else _type(row)
-                else:
-                    result = list(map(_type, cursor.fetchall()))
-            else:
-                self.database.commit()
-                if returnrowcount:
-                    result = cursor.rowcount
-                else:
-                    result = cursor.lastrowid
-        except Exception as exception:
-            cursor.close()
-            raise exception
-        cursor.close()
-        return result
-
     @property
     def _(self) -> Callable[..., Awaitable[str]]:
         """Translate something
